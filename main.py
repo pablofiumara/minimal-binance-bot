91f262fcddcdb44a2d1c81e3adebb4b099b45231
Pablo Alejandro Fiumara <pablo.fiumara@gmail.com>
2023-09-11 14:52:09 -0300

    Pass client as argument

--- main.py
+++ main.py
@@ -2,16 +2,16 @@ import os
 
 from binance.client import Client
 
-def get_moving_average(symbol, interval, window):
+def get_moving_average(client, symbol, interval, window):
     klines = client.get_historical_klines(symbol, interval, f"{window+1} day ago UTC")
     closes = [float(kline[4]) for kline in klines]
     moving_average = sum(closes[-window:]) / window
     return moving_average
 
-def execute_trades(symbol, quantity):
+def execute_trades(client, symbol, quantity):
     # get from env var 20 and 50
-    ma_20 = get_moving_average(symbol, Client.KLINE_INTERVAL_1DAY, 20)
-    ma_50 = get_moving_average(symbol, Client.KLINE_INTERVAL_1DAY, 50)
+    ma_20 = get_moving_average(client, symbol, Client.KLINE_INTERVAL_1DAY, 20)
+    ma_50 = get_moving_average(client, symbol, Client.KLINE_INTERVAL_1DAY, 50)
 
     if ma_20 > ma_50:
         # Buy
@@ -34,7 +34,7 @@ def main():
     client = Client(os.environ.get("API_KEY"), os.environ.get("API_SECRET"))
 
     while True:
-        execute_trades("BTCUSDT", "0.001")
+        execute_trades(client, "BTCUSDT", "0.001")
 
 if __name__ == '__main__':
     main()
