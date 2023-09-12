5dd0a787196f05f49e18228860ea9c0ca245c3fe
Pablo Alejandro Fiumara <pablo.fiumara@gmail.com>
2023-09-12 18:18:11 -0300

    Add tests for execute_trades method

--- tests.py
+++ tests.py
@@ -1,5 +1,5 @@
 import unittest
-from unittest.mock import MagicMock, Mock
+from unittest.mock import MagicMock, Mock, patch
 from decimal import Decimal
 
 from binance.client import Client
@@ -8,10 +8,8 @@ from main import MovingAverageStrategy
 
 class MovingAverageStrategyTests(unittest.TestCase):
     def setUp(self):
-        self.client = MagicMock(spec=Client)
         self.strategy = MovingAverageStrategy()
-
-    
+   
     def test_get_moving_average(self):
         client = Mock()
         client.get_historical_klines.return_value = [
@@ -22,10 +20,47 @@ class MovingAverageStrategyTests(unittest.TestCase):
             [0, 0, 0, 0, 5],
         ]
 
-        strategy = MovingAverageStrategy()
-        result = strategy.get_moving_average(client, 'BTCUSDT', '1d', 5)
+        result = self.strategy.get_moving_average(client, 'BTCUSDT', '1d', 5)
 
-        self.assertEqual(result, Decimal("3.0"))
+        self.assertEqual(result, Decimal("3"))
+
+    @patch("binance.client.Client")
+    def test_execute_trades(self, mocked_binance_client):
+
+        with self.subTest("should buy when fast MA value is higher than slow MA value"):
+            # side effect: [fast_MA, slow_MA]
+            self.strategy.get_moving_average = MagicMock(side_effect=[Decimal("130"), Decimal("120")])
+
+            self.strategy.execute_trades(mocked_binance_client, "BTCUSDT", "0.001", 5, 10)
+
+            mocked_binance_client.create_test_order.assert_called_with(
+                symbol="BTCUSDT",
+                side=Client.SIDE_BUY,
+                type=Client.ORDER_TYPE_MARKET,
+                quantity="0.001"
+            )
+
+        with self.subTest("should sell when fast MA value is less than slow MA value"):
+            # side effect: [fast_MA, slow_MA]
+            self.strategy.get_moving_average = MagicMock(side_effect=[Decimal("120"), Decimal("130")])
+
+            self.strategy.execute_trades(mocked_binance_client, "BTCUSDT", "0.001", 5, 10)
+
+            mocked_binance_client.create_test_order.assert_called_with(
+                symbol="BTCUSDT",
+                side=Client.SIDE_SELL,
+                type=Client.ORDER_TYPE_MARKET,
+                quantity="0.001"
+            )
+
+        mocked_binance_client.reset_mock()
+        with self.subTest("should not buy or sell when fast MA value is equal to slow MA value"):
+            # side effect: [fast_MA, slow_MA]
+            self.strategy.get_moving_average = MagicMock(side_effect=[Decimal("120"), Decimal("120")])
+
+            self.strategy.execute_trades(mocked_binance_client, "BTCUSDT", "0.001", 5, 10)
+
+            mocked_binance_client.create_test_order.assert_not_called()
 
 if __name__ == '__main__':
     unittest.main()
